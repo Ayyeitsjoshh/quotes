3c03c0bc3c852561d0a63a068d576cba07ccaee2
Ayyeitsjoshh <59943433+Ayyeitsjoshh@users.noreply.github.com>
2020-03-29 17:04:37 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,53 @@
 function myFunction() {
+  
   var myobj = document.getElementById("image");
+  
   myobj.remove();
+  
 }
+
+function alertQuote(quote) {
+
+const linebreak = '\n';
+
+// Em Dash character code
+
+const endash = '\u2014';
+
+const text = `${quote.body} ${linebreak} ${endash} ${quote.author}`;
+
+// Alert quote and credit author
+
+alert(text);
+
+}
+
+
+
+async function fetchQuote() {
+
+// API URL
+
+const url= 'https://favqs.com/api/qotd';
+
+// Call API
+
+const response = await fetch(url);
+
+
+
+// Wait for promise to resolve and return a JSON object from API response
+
+return await response.json();
+
+}
+
+
+
+// Trigger API call
+
+fetchQuote()
+
+// Handle API response
+
+.then((data) => alertQuote(data.quote));
